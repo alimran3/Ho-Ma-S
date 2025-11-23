f4d0dd83932bbfb85a05dc4ab588e5da3f31e78a
Moumita <alispower77@gmail.com>
2025-11-23 22:20:11 +0600

    kalke oo

--- project-Backend/server.js
+++ project-Backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://ho-ma-s-w1o2.vercel.app/',
   credentials: true
 }));
 app.use(express.json());
